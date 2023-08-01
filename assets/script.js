97bc0cd04418a342909717d9372dd540e91fe4a4
anrwn <andrenrwn@users.noreply.github.com>
2023-08-01 11:20:12 +0800

    Modified javascript to print random topics to console

--- assets/script.js
+++ assets/script.js
@@ -3,25 +3,35 @@ var topics = "HTML, CSS, Git, JavaScript";
 console.log(topics);
 */
 
-/*
-var topic = "HTML";
-
-if (topic === 'HTML') {
-  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-  console.log("Let's study JavaScript!");
-} else {
-  console.log('Please try again!');
-}
-*/
 
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+var topic = randomTopic;
+/* var topic = "HTML"; */
 
-for (var i = 0; i < topics.length; i++) {
-    console.log(topics[i]);
+console.log('Here are the topics we learned through Prework:');
+function listTopics() {
+    for (var x = 0; x < topics.length; x++) {
+      console.log(topics[x]);
+    }
 }
+
+listTopics();
+
+console.log('Which topic should we study first?');
+function selectTopic() {
+    if (topic === 'HTML') {
+        console.log("Let's study HTML!");
+    } else if (topic === 'CSS') {
+        console.log("Let's study CSS!");
+    } else if (topic === 'Git') {
+        console.log("Let's study Git!");
+    } else if (topic === 'JavaScript') {
+        console.log("Let's study JavaScript!");
+    } else {
+        console.log('Please try again!');
+    }
+}
+
+selectTopic();
 
